d93fa3e76f5cdb20209795fdaf477b08cdaf3105
Abe Dolinger <a.d@256hz.com>
2020-10-16 21:55:54 -0700

    Support for ?. operator, ignore comments

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 This extension automatically swaps the logic in a ternary expression for you.  It will preserve formatting/whitespace, and a semicolon at the end, if included.
 
-It supports nested ternaries and will ignore the `?` and `:` characters if they are included as part of a string.  It will show an error if the ternary won't resolve (unbalanced if/then).
+Supports nested ternaries.  `?` and `:` are ignored in strings.  `//` and `/* */` comments are ignored.  It will show an error if the ternary won't resolve (unbalanced if/then, unterminated string).
 
 ## Demos
 
@@ -21,10 +21,15 @@ It supports nested ternaries and will ignore the `?` and `:` characters if they 
 
 ## Known Issues
 
-- Quotes in comments are not ignored, and an odd number of them will mess up the parse.
+- Comments are grouped with the condition that precedes them, so they will not switch with the true/false cases.
 - Testing that the error message is shown is not done, pending finding a way to mock some VS Code internals properly.
 
 ## Release Notes
+
+### 0.2.0
+
+- Add support for Typscript `?.` operator
+- Ignore `//` and `/* */` comments (fix single quotes in comments breaking parse)
 
 ### 0.1.7
 
--- src/extension.ts
+++ src/extension.ts
@@ -19,10 +19,22 @@ type Expression = {
 const regex = {
 	quotes: /['"\`]/g,
 	spaceChar: /\s/g,
-	endSpace: /\s+$/g,
+  endSpace: /\s+$/g,
+  singleLineComment: /^[\s]{0,}[/][/].+/g,
+  singleLineBlockComment: /.{0,}[/][*].{0,}[*][/][\s]{0,}/g,
+  blockCommentBegin: /.{0,}[/][*].{0,}/g,
+  blockCommentEnd: /.{0,}[*][/].{0,}/g,
 };
 
+enum CommentResult {
+  true,
+  false,
+  endBlock,
+}
+
 export const swapTernary = (selection: string) => {
+  const selectionArray = [ ...selection ];
+
 	const expression: Expression = {
 		[Part.condition]: [] as string[],
 		[Part.trueCaseFormatting]: [] as string[],
@@ -38,17 +50,23 @@ export const swapTernary = (selection: string) => {
 	const quoteStack = [] as string[];
 	// we are matching `?`s with `:`s, so we don't need to keep the characters, just the depth
   let ternaryStackDepth = 0;
-  // oversees which part of the expression we are parsing.  Advanced according to logic below.
-	let activePart: Part = Part.condition;
+  // oversees which part of the expression we are parsing
+  let activePart: Part = Part.condition;
+  // tracks if we are inside a comment, so we can ignore characters
+  const insideComment = {
+    single: false,
+    block: false,
+  };
+  // hack to get the end of a block comment
+  let skipNext = false;
 
   // processes whitespace at the end of sections.  Advances to nextPartName on a non-space character
 	const advancePartAtEndOfFormatting = (
-		currentPartName: Part,
 		nextPartName: Part,
 		currentChar: string,
 	) => {
 		if (currentChar.match(regex.spaceChar)) {
-			expression[currentPartName].push(currentChar);
+			expression[activePart].push(currentChar);
 		} else {
 			activePart = nextPartName;
 			expression[activePart].push(currentChar);
@@ -62,68 +80,153 @@ export const swapTernary = (selection: string) => {
         ? quoteStack.pop()
         : quoteStack.push(currentChar);
 		}
-	};
+  };
 
-	selection.split('').forEach(char => {
+  const isInsideComment = (currentChar: string, index: number) => {
+    // end single
+    if (insideComment.single && currentChar === '\n') {
+      insideComment.single = false;
+      return CommentResult.false;
+    }
+
+    // end block
+    if (insideComment.block && currentChar === '*' && selectionArray[index + 1] === '/') {
+      insideComment.block = false;
+      return CommentResult.endBlock;
+    }
+
+    // start single
+    if (currentChar === '/' && selectionArray[index + 1] === '/') {
+      insideComment.single = true;
+      return CommentResult.true;
+    }
+
+    // start block
+    if (currentChar === '/' && selectionArray[index + 1] === '*') {
+      insideComment.block = true;
+      return CommentResult.true;
+    }
+
+    return Object.values(insideComment).some(x => x)
+      ? CommentResult.true
+      : CommentResult.false;
+  };
+
+
+	selectionArray.forEach((char, index) => {
 		switch (activePart) {
-			case Part.condition:
+			case Part.condition: {
+        if (skipNext) {
+          expression[activePart].push(char);
+          skipNext = false;
+          break;
+        }
+
+        const inComment = isInsideComment(char, index);
+        if (inComment === CommentResult.true) {
+          expression[activePart].push(char);
+					break;
+        }
+
+        if (inComment === CommentResult.endBlock) {
+          expression[activePart].push(char);
+          skipNext = true;
+          break;
+        }
+
 				handleIfQuote(char);
 
 				if (quoteStack.length) {
 					expression[activePart].push(char);
 					break;
 				}
-	
-				if (char === '?' && !quoteStack.length) {
+
+				if (char === '?' && !quoteStack.length && selectionArray[index + 1] !== '.') {
 					activePart = Part.trueCaseFormatting;
 					ternaryStackDepth = 1;
 					break;
 				}
-	
+
 				expression[activePart].push(char);
 				break;
+      }
 
 			case Part.trueCaseFormatting:
 				handleIfQuote(char);
 				advancePartAtEndOfFormatting(
-					Part.trueCaseFormatting, 
 					Part.trueCase,
 					char,
 				);
 				break;
 
-			case Part.trueCase:
+			case Part.trueCase: {
+        if (skipNext) {
+          expression[activePart].push(char);
+          skipNext = false;
+          break;
+        }
+
+        const inComment = isInsideComment(char, index);
+        if (inComment === CommentResult.true) {
+          expression[activePart].push(char);
+					break;
+        }
+
+        if (inComment === CommentResult.endBlock) {
+          expression[activePart].push(char);
+          skipNext = true;
+          break;
+        }
+
 				handleIfQuote(char);
-		
+
 				if (quoteStack.length) {
 					expression[activePart].push(char);
 					break;
 				}
-		
-				if (char === '?') {
+
+				if (char === '?' && selectionArray[index + 1] !== '.') {
 					ternaryStackDepth += 1;
 				} else if (char === ':') {
 					ternaryStackDepth -= 1;
 				}
-		
+
 				if (ternaryStackDepth === 0) {
 					activePart = Part.falseCaseFormatting;
 					break;
 				}
-		
+
 				expression[activePart].push(char);
 				break;
+      }
 
 			case Part.falseCaseFormatting:
 				handleIfQuote(char);
 				advancePartAtEndOfFormatting(
-					Part.falseCaseFormatting,
 					Part.falseCase,
 					char,
 				);
 				break;
 
 			case Part.falseCase:
+        if (skipNext) {
+          expression[activePart].push(char);
+          skipNext = false;
+          break;
+        }
+
+        const inComment = isInsideComment(char, index);
+        if (inComment === CommentResult.true) {
+          expression[activePart].push(char);
+					break;
+        }
+
+        if (inComment === CommentResult.endBlock) {
+          expression[activePart].push(char);
+          skipNext = true;
+          break;
+        }
+
 				handleIfQuote(char);
 				expression[activePart].push(char);
 				break;
@@ -135,17 +238,17 @@ export const swapTernary = (selection: string) => {
 	if (ternaryStackDepth !== 0) {
     const neededNumber = Math.abs(ternaryStackDepth);
     const usePlural = neededNumber !== 1;
-		expression.errors.push(`Error parsing ternary: need ${neededNumber} more \`${ternaryStackDepth < 0 ? '?' : ':'}\`${usePlural ? 's' : ''}`);
+		expression.errors.push(`Need ${neededNumber} more \`${ternaryStackDepth < 0 ? '?' : ':'}\`${usePlural ? 's' : ''}`);
   }
 
   if (!expression[Part.trueCase].length || !expression[Part.falseCase].length) {
-    expression.errors.push('Could not parse selection as ternary');
+    expression.errors.push('Empty true or false case');
   }
 
   if (quoteStack.length > 0) {
-    expression.errors.push('Could not parse terminary - unterminated string');
+    expression.errors.push('Unterminated string');
   }
-  
+
   return expression;
 };
 
@@ -164,10 +267,10 @@ export const formatExpression = (expression: Expression) => {
   const falseCaseFormatting = expression[Part.falseCaseFormatting].join('');
 
   const falseCase = hasSemicolonEnd
-    ? endOfOriginalExpression.split('').slice(0, -1).join('')
+    ? [ ...endOfOriginalExpression ].slice(0, -1).join('')
     : endOfOriginalExpression;
 
-    const falseCaseEndFormatting = expression[Part.falseCase].join('').match(regex.endSpace)?.join('');
+    const falseCaseEndFormatting = expression[Part.falseCase].join('').match(regex.endSpace)?.join('') || '';
 
   return (
     condition
@@ -188,7 +291,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('256hz.swapTernary', () => {
 		const activeEditor = vscode.window.activeTextEditor;
-		
+
 		if (!activeEditor) {
 			return;
 		}
@@ -205,8 +308,10 @@ export function activate(context: vscode.ExtensionContext) {
       const newExpression = swapTernary(expression);
 
       if (newExpression.errors.length) {
-        newExpression.errors.forEach(error => {
-			    vscode.window.showErrorMessage(error);
+        vscode.window.showErrorMessage(newExpression.errors[0]);
+
+        activeEditor.edit(editBuilder => {
+          editBuilder.replace(selection, JSON.stringify(newExpression, null, 2));
         });
         return;
       }
@@ -214,7 +319,6 @@ export function activate(context: vscode.ExtensionContext) {
 			activeEditor.edit(editBuilder => {
 				editBuilder.replace(selection, formatExpression(newExpression));
 			});
-			
 	});
 
 	context.subscriptions.push(disposable);
@@ -223,3 +327,23 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
   console.log('SwapTernary deactivated');
 }
+
+/* test statements
+
+x ? y : z
+
+x
+  ? y
+  : z;
+
+object?.withProperty?.thing
+  ? rejoice()
+  : weep();
+
+x
+  // commen't
+  ? things
+  // comment'n't? '
+  : other things
+
+*/
